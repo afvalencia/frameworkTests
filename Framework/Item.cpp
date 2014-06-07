f64aa768001de5663eb0072c6b1153f77a5861e5
Andrés Valencia <felipesandovalv@hotmail.com>
2014-06-07 18:50:03 -0500

    Clase Item
    Clase Item completa

--- Framework/Item.cpp
+++ Framework/Item.cpp
@@ -5,10 +5,16 @@ using namespace std;
 class Item
 {
 	Categoria categoria;
+	string nombre;
+	int ancho;
+	int alto;
+
 public:
-	Item(Categoria categoria)
+	Item(string nombre, int ancho, int alto)
 	{
-		this->categoria = categoria;
+		categoria = NULL;
+		this->ancho = ancho;
+		this->alto = alto;
 	}
 
 	void setCategoria(Categoria categoria)
@@ -16,4 +22,18 @@ public:
 		this->categoria = categoria;
 	}
 	
+	void setNombre(string nombre) 
+	{
+		this->nombre = nombre;
+	}
+
+	void setAncho(int ancho) 
+	{
+		this->ancho = ancho;
+	}
+
+	void setAlto(int alto)
+	{
+		this->alto = alto;
+	}
 };
